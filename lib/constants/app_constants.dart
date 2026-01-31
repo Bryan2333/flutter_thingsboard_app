9e6532127a7f0cbec6ad47d87bd499d2bc71a418
BryanLiang <liangrui.ch@gmail.com>
2026-01-31 13:31:11 +0800

    change api endpoint address

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,5 @@
 abstract final class ThingsboardAppConstants {
-  static const thingsBoardApiEndpoint = String.fromEnvironment('thingsboardApiEndpoint');
+  static const thingsBoardApiEndpoint = 'http://192.168.9.121:8080';
   static const thingsboardOAuth2CallbackUrlScheme = String.fromEnvironment('thingsboardOAuth2CallbackUrlScheme');
   static const thingsboardIOSAppSecret = String.fromEnvironment('thingsboardIosAppSecret');
   static const thingsboardAndroidAppSecret = String.fromEnvironment('thingsboardAndroidAppSecret');
